d08fd91fafe68dd13cfca75b1f11197504a1f31e
huwfernie <huwfernie@gmail.com>
2022-11-13 15:14:10 +0000

    working on localhost

--- src/js/main.js
+++ src/js/main.js
@@ -3,37 +3,52 @@ import '../scss/styles.scss';
 
 // Import all of Bootstrap's JS
 // import * as bootstrap from 'bootstrap';
+
 class App {
   constructor() {
 
     const el = document.querySelector('[data-button-form-submit]');
     el.addEventListener('click', handleClick);
 
-    function handleClick(ev) {
-      ev.preventDefault();
-      const formEl = findForm(ev.target);
-      const formData = new FormData(formEl);
-      const request = new XMLHttpRequest();
-      request.onload = (ev) => {
-        console.log(ev);
+    async function handleClick(ev) {
+      try {
+        ev.preventDefault();
+        const formEl = findForm(ev.target);
+        const responseEl = formEl.querySelector('.response');
+        const formData = new FormData(formEl);
+        const data = {
+          type: formData.get('type'),
+          updates: formData.get('updates'),
+          email: formData.get('email')
+        }
+        const url = 'https://mx7v8u910g.execute-api.eu-west-1.amazonaws.com/items';
+        const response = await fetch(url, {
+          method: 'PUT',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+
+        if (response.status === 200) {
+          responseEl.innerText = 'You\'ve been added to our email list';
+        } else {
+          responseEl.innerText = 'Something went wrong, please try again later, or drop us an email at hello@floit.app';
+        }
+        const resData = await response.json();
+      } catch (error) {
+        console.log(error);
       }
-      // request.open("PUT", "https://bwe9las6e7.execute-api.eu-west-1.amazonaws.com/default/readWriteDynamoDB/");
-      request.open("POST", "https://mx7v8u910g.execute-api.eu-west-1.amazonaws.com/items");
-      request.setRequestHeader('Content-type', 'application/json');
-      // const testData = JSON.parse({id: '987654', price: '12345', name: 'myitem'});
-      const json = '{"id":true, "count":42}';
-      const obj = JSON.parse(json);
-      request.send(obj);
     }
 
-    function findForm(el) {
-      if (el.nodeName === 'FORM') {
-        return el;
-      } else {
-        return findForm(el.parentElement);
+      function findForm(el) {
+        if (el.nodeName === 'FORM') {
+          return el;
+        } else {
+          return findForm(el.parentElement);
+        }
       }
     }
   }
-}
 
-const app = new App;
+  const app = new App;
